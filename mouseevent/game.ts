c5f5b5137efff192a7bfb6987a2ea2ad1f61cca9
Mori <823779508@qq.com>
2016-04-07 13:17:09 +0800

    123

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -1,13 +1,37 @@
 
 var humanContainer = new render.DisplayObjectContainer();
-var head = new render.Bitmap();
-head.x = 100;
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
+var Container = new render.DisplayObjectContainer();
+humanContainer.x = 150;
+humanContainer.y = 250;
 
+humanContainer.addChild(Container);
+Container.y = -210;
+Container.x = -120;
+
+var head = new render.Bitmap();
+var leftarm = new render.Bitmap();
+var rightarm = new render.Bitmap();
+var leftleg = new render.Bitmap();
+var rightleg = new render.Bitmap();
+var Hbody = new render.Bitmap();
+
+
+head.source = "head.png";
+leftarm.source = "leftarm.png";
+rightarm.source = "rightarm.png";
+leftleg.source = "leftleg.png";
+rightleg.source = "rightleg.png";
+Hbody.source = "body.png";
+
+Container.addChild(head)
+Container.addChild(leftarm)
+Container.addChild(rightarm)
+Container.addChild(leftleg)
+Container.addChild(rightleg)
+Container.addChild(Hbody)
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png", "leftarm.png", "rightarm.png", "leftleg.png", "rightleg.png", "body.png"]);
 
 class HumanBody extends Body {
     
@@ -16,8 +40,10 @@ class HumanBody extends Body {
     
 
     onTicker(duringTime: number) {
-        this.x = 100;//+= duringTime * this.vx;
-        this.y = 100;
+        body.vx = 5;
+        this.x += this.vx * duringTime;
+        this.y = 200;
+        this.rotation += Math.PI * duringTime;
 
     }
 }
