48b14f6262c8c0a2a6ac97b481806f1db5e552c8
smibatah <1255640109@qq.com>
2016-04-26 15:04:55 +0800

    game

--- game/data.js
+++ game/data.js
@@ -1,58 +1,41 @@
-
-module data {
-
-    export class Storage {
-
-        private static _instance: Storage;
-
-        public static getInstance(): Storage {
+var data;
+(function (data) {
+    var Storage = (function () {
+        function Storage() {
+        }
+        Storage.getInstance = function () {
             if (Storage._instance == null) {
                 Storage._instance = new Storage();
             }
             return Storage._instance;
-        }
-
-
-        public readFile() {
+        };
+        Storage.prototype.readFile = function () {
             this.mapData = null;
             this.startRequest();
             /*
             var content = fs.readFileSync(map_path, "utf-8");
             var obj = JSON.parse(content);
             this.mapData = obj.map;*/
-        }
-        
-        //public saveFile(){
-            
-        //}
-        
-        private xmlHttp;
-        public mapData;
-        
-        private startRequest()
-        {
+        };
+        Storage.prototype.startRequest = function () {
             this.xmlHttp = new XMLHttpRequest();
-            try
-            {
+            try {
                 this.xmlHttp.open("GET", "map.json", false);
                 this.xmlHttp.send(null);
                 this.xmlHttp.onreadystatechange = this.handleStateChange();
             }
-            catch(exception)
-            {
+            catch (exception) {
                 alert("XMLHttp Fail");
             }
-        }
-        private handleStateChange()
-        {
-            if(this.xmlHttp.readyState == 4)
-            {
-                if (this.xmlHttp.status == 200 || this.xmlHttp.status == 0)
-                {
+        };
+        Storage.prototype.handleStateChange = function () {
+            if (this.xmlHttp.readyState == 4) {
+                if (this.xmlHttp.status == 200 || this.xmlHttp.status == 0) {
                     this.mapData = JSON.parse(this.xmlHttp.responseText).map;
                 }
             }
-        }
-        
-    }
-}
+        };
+        return Storage;
+    }());
+    data.Storage = Storage;
+})(data || (data = {}));
